84c8b9ea9e68af91bfbd372b42cfdf2e2fe8054a
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2024-02-18 00:24:22 +0100

    copy rust impl from shuttle example and use cargo workspace

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tower_http::services::{ServeDir, ServeFile};
+
+#[shuttle_runtime::main]
+async fn axum() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().nest_service(
+        "/",
+        ServeDir::new("ui/dist").not_found_service(ServeFile::new("ui/dist/index.html")),
+    );
+
+    Ok(router.into())
 }
