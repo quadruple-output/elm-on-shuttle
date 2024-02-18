02b502047210f3fdd71244cd53bd4c88066a9a1d
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2024-02-18 19:16:55 +0100

    provide a simple server greeting

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,18 @@
+use axum::routing::get;
 use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().nest_service(
+    let api_router = Router::new().route("/greet", get(greet));
+    let router = Router::new().nest("/api", api_router).nest_service(
         "/",
         ServeDir::new("ui/dist").not_found_service(ServeFile::new("ui/dist/index.html")),
     );
 
     Ok(router.into())
+}
+
+async fn greet() -> &'static str {
+    "Hello from the server"
 }
