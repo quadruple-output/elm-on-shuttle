4923ea35ccb6844f9cf2ff30e560f98c40aab339
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2024-02-25 15:58:01 +0100

    clean-up Home_.elm

--- ui/src/Pages/Home_.elm
+++ ui/src/Pages/Home_.elm
@@ -11,37 +11,14 @@ page : Page Model Msg
 page =
     Page.element
         { init = init
+        , subscriptions = \_ -> Sub.none
         , update = update
-        , subscriptions = subscriptions
         , view = view
         }
 
 
-
--- INIT
-
-
 type alias Model =
     { greeting : Maybe (Result Http.Error String) }
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( { greeting = Nothing }
-    , server_api_get_greeting { onResponse = ReceivedGreeting }
-    )
-
-
-server_api_get_greeting : { onResponse : Result Http.Error String -> msg } -> Cmd msg
-server_api_get_greeting params =
-    Http.get
-        { url = "/api/greet"
-        , expect = Http.expectString params.onResponse
-        }
-
-
-
--- UPDATE
 
 
 type Msg
@@ -49,39 +26,31 @@ type Msg
     | ReceivedGreeting (Result Http.Error String)
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( { greeting = Nothing }
+    , Http.get
+        { url = "/api/greet"
+        , expect = Http.expectString ReceivedGreeting
+        }
+    )
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            ( model
-            , Cmd.none
-            )
+            ( model, Cmd.none )
 
         ReceivedGreeting greeting ->
-            ( { model | greeting = Just greeting }
-            , Cmd.none
-            )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
-
-
-
--- VIEW
+            ( { model | greeting = Just greeting }, Cmd.none )
 
 
 view : Model -> View Msg
 view model =
     { title = "Elm on Shuttle"
     , attributes = []
-    , element =
-        el [ centerX, centerY ] <| show_greeting model.greeting
+    , element = el [ centerX, centerY ] <| show_greeting model.greeting
     }
 
 
