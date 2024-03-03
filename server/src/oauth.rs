8ab852464321f62ed8c81e8c16fd28120edc2819
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2024-03-03 01:12:57 +0100

    feat: server gets token from github

--- server/src/oauth.rs
+++ server/src/oauth.rs
@@ -1,7 +1,9 @@
-use ::axum::extract::Query;
-use ::axum::{routing::get, Router};
-use ::serde::Deserialize;
+use ::axum::{extract::Query, routing::get, Router};
+use ::reqwest;
+use ::serde::{Deserialize, Serialize};
+use ::std::collections::HashMap;
 use ::tracing::debug;
+use axum::Json;
 
 pub(crate) fn router() -> Router<()> {
     Router::new().route("/callback/github", get(github_callback))
@@ -13,13 +15,51 @@ struct CallbackParams {
     state: Option<String>,
 }
 
-async fn github_callback(params: Query<CallbackParams>) -> String {
-    let info = match params.0 {
-        CallbackParams{code, state:Some(state)} => 
-            format!("code: {code}, state: {state}"),
-        CallbackParams{code, state:None} =>
-            format!("code: {code}, no state"),
+#[derive(Default, Serialize)]
+struct TokenResponse {
+    server_status: Option<String>,
+    error_message: Option<String>,
+    raw_body: Option<String>,
+}
+
+async fn github_callback(params: Query<CallbackParams>) -> Json<TokenResponse> {
+    let CallbackParams { code, state } = params.0;
+    let info = match (&*code, state) {
+        (code, Some(state)) => format!("code: {code}, state: {state}"),
+        (code, None) => format!("code: {code}, no state"),
     };
     debug!("{info}");
-    info.to_string()
+
+    let mut map = HashMap::new();
+    map.insert("client_id", "Iv1.b5ba4dcd32da9063".to_string());
+    map.insert(
+        "client_secret",
+        "".to_string(), // !!!!!!!!!!!!!!!!!!!!!!! TODO
+    );
+    map.insert("code", code.to_string());
+    map.insert("redirect_uri", "".to_string());
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post("https://github.com/login/oauth/access_token")
+        .header(reqwest::header::ACCEPT, "application/json")
+        .json(&map)
+        .send()
+        .await;
+
+    let mut out = TokenResponse::default();
+    match response {
+        Ok(response) => {
+            let status = response.status();
+            let status_reason = status.canonical_reason().unwrap_or("<unknown>");
+            out.server_status = Some(format!("{status}: {status_reason}"));
+            let body = response.text().await;
+            match body {
+                Ok(body) => out.raw_body = Some(body),
+                Err(e) => out.error_message = Some(format!("Error receiving response body: {e}")),
+            }
+        }
+        Err(e) => out.error_message = Some(format!("Error connecting to token service: {e}")),
+    }
+    Json(out)
 }
